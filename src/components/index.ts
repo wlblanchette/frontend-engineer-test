e93732e72f5cf27ed0fc0c39d75c1ea67431567b
Errol Silver <errol.silver@cloudzero.com>
2022-03-08 14:47:22 -0500

    add license and headers

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2016-present, CloudZero, Inc. All rights reserved.
+// Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
 export { ProductTable } from './product-table'
 export { Search } from './search'
 export { Pagination } from './pagination'
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,3 +1,5 @@
+// Copyright (c) 2016-present, CloudZero, Inc. All rights reserved.
+// Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
 import React, { FunctionComponent } from 'react'
 import "./search.css"
 
